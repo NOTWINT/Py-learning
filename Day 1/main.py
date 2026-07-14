43b203511c17f7574694378c9038255e22adb0ee
notwint <shuklaparas048@outlook.com>
2026-07-14 15:29:04 +0530

    Just a little bit of smard work:)

---
+++ Day 1/main.py
@@ -0,0 +1,2 @@
+a=b=c=15+20j
+print(a, b, c)
---
+++ Day 2/2nd.py
@@ -0,0 +1,8 @@
+n1=input("Enter 1st number")
+n2=input("Enter 2nd number")
+if n1 > n2:
+    print("1st number is bigger")
+else:
+    print("2nd number is bigger")
+for i in range(3):
+    print("Meowwww")
